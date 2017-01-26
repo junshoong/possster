899764ae4a009630dee8076241d8b3deb2ad1d30
harvey <vaporize93@gmail.com>
2017-01-26 15:29:19 +0900

    poster 모델 테스트작성 및 모델 편집

--- possster/poster/models.py
+++ possster/poster/models.py
@@ -3,21 +3,15 @@ from django.contrib.auth.models import User
 from datetime import datetime
 
 
-class PosterCategory(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
-
-
 class Poster(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='poster')
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(PosterCategory, null=True, blank=True)
     content = models.TextField(max_length=5000, null=True, blank=True)
-
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     end = models.DateTimeField(null=True, blank=True)
 
     @property
     def is_over(self):
-        return self.end > datetime.now()
+        return self.end < datetime.now()
--- possster/poster/tests.py
+++ possster/poster/tests.py
@@ -1,20 +1,56 @@
 from django.test import TestCase
-from poster.models import PosterCategory
+from possster.settings import MEDIA_ROOT
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.contrib.auth.models import User
+from poster.models import Poster
+from datetime import datetime
+from datetime import timedelta
+import glob
+import os
 
 
-class PosterCategoryTest(TestCase):
+class PosterTest(TestCase):
 
-    def test_saving_and_retrieving_poster_category(self):
-        first_category = PosterCategory()
-        first_category.name = 'first'
-        first_category.save()
+    _image = SimpleUploadedFile(name='test_image.jpg', content=None, content_type='image/jpeg')
 
-        second = PosterCategory()
-        second.name = 'second'
-        second.save()
+    @staticmethod
+    def _create_user():
+        user = User.objects.create(username='test')
+        return user
 
-        saved_category = PosterCategory.objects.all()
-        self.assertEqual(saved_category.count(), 2)
+    def test_saving_and_retrieving_poster(self):
+        user = self._create_user()
+        first_poster = Poster()
+        first_poster.title = 'first'
+        first_poster.image = self._image
+        first_poster.writer = user
+        first_poster.save()
 
-        self.assertEqual(saved_category[0].name, 'first')
-        self.assertEqual(saved_category[1].name, 'second')
+        second_poster = Poster()
+        second_poster.title = 'second'
+        second_poster.image = self._image
+        second_poster.writer = user
+        second_poster.save()
+
+        saved_poster = Poster.objects.all()
+        self.assertEqual(saved_poster.count(), 2)
+        self.assertEqual(saved_poster[0].title, 'first')
+        self.assertEqual(saved_poster[1].title, 'second')
+        self.assertEqual(saved_poster[0].writer, user)
+
+    def test_expired_poster(self):
+        user = self._create_user()
+        dt = timedelta(days=1)
+        yesterday = datetime.now() - dt
+        tomorrow = datetime.now() + dt
+
+        expired_poster = Poster.objects.create(title='test_poster', image=self._image, writer=user, end=yesterday)
+        self.assertTrue(expired_poster.is_over)
+
+        expired_poster = Poster.objects.create(title='test_poster2', image=self._image, writer=user, end=tomorrow)
+        self.assertFalse(expired_poster.is_over)
+
+    def tearDown(self):
+        for f in glob.glob(MEDIA_ROOT+'/poster/test_image*.jpg'):
+            os.remove(f)
+
