1438f5c1da5c8128e4693deb968a1d3a841dcdb9
harvey <vaporize93@gmail.com>
2017-01-26 16:44:22 +0900

    최초화면인 HomeView와 기능 테스트 초안 작성

--- possster/possster/urls.py
+++ possster/possster/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from possster.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^$', HomeView.as_view(), name='home'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
