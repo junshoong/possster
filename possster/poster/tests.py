7a0cccde98de7d9fd5f25d6594198779ffb49e0b
harvey <vaporize93@gmail.com>
2017-01-27 23:39:11 +0900

    PosterLV에 대한 테스트 통합 정리

--- possster/poster/tests.py
+++ possster/poster/tests.py
@@ -1,11 +1,9 @@
 from django.test import TestCase
 from django.test import override_settings
-from django.test.client import RequestFactory
+from django.test.client import Client
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.core.urlresolvers import resolve
 from django.contrib.auth.models import User
 from poster.models import Poster
-from poster.views import PosterLV
 from datetime import datetime
 from datetime import timedelta
 
@@ -64,19 +62,8 @@ class PosterModelTest(TestCase):
 
 class PosterLVTest(TestCase):
     def setUp(self):
-        self.factory = RequestFactory()
+        self.client = Client()
 
-    def test_root_url_resolves_to_poster_list_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func.__name__, PosterLV.__name__)
-
-    def test_poster_list_page_returns_correct_html(self):
-        request = self.factory.get('/')
-        request.session = {}
-        view_func = PosterLV.as_view()
-        response = view_func(request)
-        response.render()
-        html = response.content.decode('utf8')
-        self.assertIn('<title>Possster</title>', html)
-        self.assertTrue(html.endswith('</html>'))
-
+    def test_uses_poster_list_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'poster/poster_list.html')
