d764a937a2c44c1d98cbf5970bda3b8c07f94ace
harvey <vaporize93@gmail.com>
2017-01-28 22:34:30 +0900

    Poster 작성에 대한 테스트 케이스 및 코드 작성

--- possster/poster/views.py
+++ possster/poster/views.py
@@ -1,13 +1,21 @@
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
+from django.core.urlresolvers import reverse_lazy
 from poster.models import Poster
 
 
 class PosterLV(ListView):
     model = Poster
     template_name = 'poster/poster_list.html'
+    context_object_name = 'poster'
 
 
 class PosterCV(CreateView):
     model = Poster
-    fields = ('title', 'image', 'end')
+    fields = ('title', 'image', 'end', 'writer')
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        if self.request.user.is_authenticated():
+            form.instance.writer = self.request.user
+        return super(PosterCV, self).form_valid(form)
