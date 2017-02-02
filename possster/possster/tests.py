82a63bb8b41f79d401e60f0fe1703f0eb4c7aac7
harvey <vaporize93@gmail.com>
2017-02-02 13:49:30 +0900

    사용자 form 테스트 추가

---
+++ possster/possster/tests.py
@@ -0,0 +1,58 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from possster.forms import CustomUserCreationForm
+
+
+class SetupClass(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username="tester",
+            password="qq12341234",
+            email="tester@example.com",
+        )
+
+
+class CustomUserCreationFormTest(TestCase):
+    def test_form_valid(self):
+        form = CustomUserCreationForm(data={
+            'username': 'tester',
+            'email': 'tester@example.com',
+            'password1': 'qq12341234',
+            'password2': 'qq12341234',
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_form_invalid(self):
+        form = CustomUserCreationForm(data={
+            'username': 'tester',
+            'email': '',
+            'password1': 'qq',
+            'password2': 'qq',
+        })
+        self.assertFalse(form.is_valid())
+
+
+class UserCreationViewTest(SetupClass):
+    def test_uses_register_template(self):
+        response = self.client.get('/register/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/register.html')
+
+    def test_add_user_invalid_form_view(self):
+        response = self.client.post('/register/', {
+            'username': 'tester',
+            'email': 'tester@example.com',
+            'password': 'qq12341234',
+        })
+        self.assertTrue("이미 존재", response.content.decode('utf-8'))
+
+    def test_add_user_valid_form_view(self):
+        user_count = User.objects.count()
+        response = self.client.post('/register/', {
+            'username': 'tester2',
+            'email': 'tester2@example.com',
+            'password1': 'qq12341234',
+            'password2': 'qq12341234',
+        }, follow=True)
+        self.assertRedirects(response, '/register/done')
+        self.assertEqual(User.objects.count(), user_count+1)
