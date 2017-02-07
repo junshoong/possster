47e3ffc3bbcb8488a6d5ae3c7f0a02f824791302
Harvey <vaporize93@gmail.com>
2017-02-07 15:48:31 +0900

    Docker를 사용한 AWS 배포  (#20)

---
+++ possster/possster/production.py
@@ -0,0 +1,19 @@
+from possster.settings import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['possster.harveyk.me', ]
+
+STATIC_ROOT = '/srv/static/'
+
+#Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+     
